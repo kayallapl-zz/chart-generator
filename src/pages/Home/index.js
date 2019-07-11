111290d53051cbd5507756bb084b9b106cddadbc
Kayalla <kayallapl@id.uff.br>
2019-07-10 23:47:24 -0300

    created Home component

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>Olá</div>
+    );
+  }
+}
